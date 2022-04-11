81fb74b8a991086fdc1b6ad61e1fea0cd77de3e4
Roman Pavlov <pra_dev@mail.ru>
2022-04-12 03:31:44 +0500

    Add some char fields max length, make migration

--- book_site/books/models.py
+++ book_site/books/models.py
@@ -9,13 +9,13 @@ from django.db import models
 class Books(models.Model):
     """ Book model """
 
-    title = models.CharField(max_length=30, verbose_name='Название')
+    title = models.CharField(max_length=150, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
-    cover = models.ImageField(upload_to='media/covers/%Y/%m/%d', blank=True, verbose_name='Название')
+    cover = models.ImageField(upload_to='media/covers/%Y/%m/%d', blank=True, verbose_name='Обложка')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата загрузки')
     publish_year = models.DecimalField(max_digits=4, decimal_places=0, verbose_name='Дата публикации')
     pages = models.DecimalField(max_digits=6, decimal_places=0, verbose_name='Кол-во страниц')
-    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Название')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
     rating = models.DecimalField(default=0, max_digits=2, decimal_places=1, verbose_name='Рейтинг')
     downloads = models.IntegerField(default=0, verbose_name='Скачиваний')
     age_restriction = models.DecimalField(
@@ -103,8 +103,8 @@ class Books(models.Model):
 class Artists(models.Model):
     """ Artist model """
 
-    first_name = models.CharField(max_length=15, verbose_name='Имя')
-    last_name = models.CharField(max_length=15, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=30, verbose_name='Имя')
+    last_name = models.CharField(max_length=30, verbose_name='Фамилия')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
     email = models.EmailField(blank=True, null=True, verbose_name='Почта')
@@ -126,7 +126,7 @@ class Artists(models.Model):
 
 class Companies(models.Model):
     """ Company model """
-    name = models.CharField(max_length=15, verbose_name='Название')
+    name = models.CharField(max_length=150, verbose_name='Название')
     email = models.EmailField(blank=True, null=True, verbose_name='Почта')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
@@ -141,7 +141,7 @@ class Companies(models.Model):
 
 class Series(models.Model):
     """ Series model """
-    name = models.CharField(max_length=15, verbose_name='Название')
+    name = models.CharField(max_length=150, verbose_name='Название')
     authors = models.ManyToManyField('Artists', related_name='author_series', verbose_name='Авторы')
     description = models.TextField(verbose_name='Описание')
 
@@ -155,7 +155,7 @@ class Series(models.Model):
 
 class Genres(models.Model):
     """ Genre model """
-    name = models.CharField(max_length=15, verbose_name='Название')
+    name = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
@@ -168,7 +168,7 @@ class Genres(models.Model):
 
 class Tags(models.Model):
     """ Tag model """
-    name = models.CharField(max_length=15, verbose_name='Название')
+    name = models.CharField(max_length=100, verbose_name='Название')
 
     def __str__(self):
         return self.name
@@ -181,7 +181,7 @@ class Tags(models.Model):
 class Commentary(models.Model):
     """ Commentary model """
 
-    ip = models.CharField(max_length=10, verbose_name='IP Адресс')
+    ip = models.CharField(max_length=50, verbose_name='IP Адресс')
     book = models.ForeignKey(
         Books,
         on_delete=models.CASCADE,
@@ -204,8 +204,7 @@ class Commentary(models.Model):
         related_name='child',
         verbose_name='Родитель'
     )
-    content = models.CharField(max_length=10, verbose_name='Содержание')
-
+    content = models.TextField(verbose_name='Содержание')
 
     def __str__(self):
         return self.book.title + self.content[:self.content.find('', 15)] + '...'
@@ -235,7 +234,7 @@ class Quote(models.Model):
 
     book = models.ForeignKey(Books, on_delete=models.CASCADE, related_name='quotes', verbose_name='Книга')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quotes', verbose_name='Пользователь')
-    content = models.CharField(max_length=10, verbose_name='Содержание')
+    content = models.TextField(verbose_name='Содержание')
     up_votes = models.IntegerField(verbose_name='Голоса за')
     down_votes = models.IntegerField(verbose_name='Голоса против')
 
@@ -255,8 +254,8 @@ class UserProfile(models.Model):
     """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
-    first_name = models.CharField(max_length=15, verbose_name='Имя')
-    last_name = models.CharField(max_length=15, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=20, verbose_name='Имя')
+    last_name = models.CharField(max_length=20, verbose_name='Фамилия')
     avatar = models.ImageField(upload_to='media/avatars/%Y/%m/%d', blank=True, verbose_name='Аватар')
 
     read_books = models.ManyToManyField(Books, symmetrical=False, related_name='+', verbose_name='Прочитано')
