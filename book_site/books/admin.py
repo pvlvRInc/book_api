cd6a61784d07c9c33ef88edd812eea94d3ad2f5e
Roman Pavlov <pra_dev@mail.ru>
2022-04-12 02:20:15 +0500

    Register models in admin, add books app

--- book_site/books/admin.py
+++ book_site/books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Books, Artists, Companies, Series, Genres, Tags, Commentary, UserBookRating, Quote, UserProfile
+
+# class BookAdmin(admin.ModelAdmin):
+#     fields =
+#     class Meta:
+#         verbose_name = 'Книга'
+#         verbose_name_plural = 'Книги'
+
+
+admin.register(Books)
+admin.register(Artists)
+admin.register(Companies)
+admin.register(Series)
+admin.register(Genres)
+admin.register(Tags)
+admin.register(Commentary)
+admin.register(UserBookRating)
+admin.register(Quote)
+admin.register(UserProfile)
